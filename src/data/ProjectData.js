3b2c60433767f25f2d45881864bb48961d3875e2
raga <21ita40@karpagamtech.ac.in>
2024-05-01 08:17:29 +0530

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,37 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/jobDetect.png",
+    title: "JobDetect",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["Python", "Flask", "NodeJs", "MongoDB"],
+    github_url: "https://github.com/salesp07/JobDetectNode",
+    demo_url: "https://jobdetective.onrender.com/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+];
